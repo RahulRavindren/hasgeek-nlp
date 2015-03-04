b4a5b7515b6df684d588f34872d629071e06f11f
Rahul Ravindran <rahul.r945@gmail.com>
2015-03-04 21:14:15 +0530

    added stop words filter and pickle dump and load functions

--- data.py
+++ data.py
@@ -1,24 +1,44 @@
+#-*- author:Rahul Ravindran
+#-*- coding:UTF-8
 import nltk
 from nltk.tokenize.punkt import PunktSentenceTokenizer as st
-import csv,re
+from nltk.corpus import stopwords
+import csv,pickle
 from bs4 import BeautifulSoup
 
 class StatPreProcessing():
 	"""
 	Pre Processing applied on the data coming from a post.
-	The noise removed are html tags and stop words.
+	noise removed : html tags,stop words
 
 	"""
+	def __init__(self):
+		self.nounSentence = []
+		self.dictClean = []
+	
 	def nounExtractor(self,sentence):
-
-		tokenizedText = nltk.word_tokenize(sentence)
-		posTagSentence = nltk.pos_tag(tokenizedText)
+		self.tokenizedText = nltk.word_tokenize(sentence)
+		posTagSentence = nltk.pos_tag(self.tokenizedText)
 		for t in posTagSentence:
-			if((t[1]== 'NNP') & (t[1] == 'NN')):
-					nounSentence = posTagSentence
-		return nounSentence
-	def tagSentenceDump(self,sent_tokenize):
-		return
+			if(t[1]== "NNP"):
+				self.nounSentence.append(posTagSentence)						
+	
+	def stopWordFilter(self):
+		stop = stopwords.words('english')
+		for i in self.nounSentence:
+			if i[0] not in stop:
+				self.dictClean.append(i[0])	
+	
+	def tagSentenceDump(self,object):
+		with open('data/sent_job_title.tab', 'wb') as file:
+				pickle.dump(object,file)
+		file.close()
+
+	def loadPickleDump(self,pickleFileName):
+		temp = pickle.load(open(pickleFileName,'rb'))
+		return temp
+
+	
 class DataStripper():		
 		"""
 		Returns data in dictionary format 
@@ -34,7 +54,7 @@ class DataStripper():
 			with open(filePath,'rw') as file:
 				reader  = csv.reader(file,delimiter=',')
 				for row in reader:
-					self.htmlTagStripper(row[2])
+					self.htmlTagStripper(row[3])
 					#self.tagCleanedrow3.append(temprow3)
 					# temprow2 = self.htmlTagStripper(row[2])
 					# self.tagCleanedrow2.append(temprow2)
@@ -45,7 +65,7 @@ class DataStripper():
 			"""
 			soup = BeautifulSoup(data)
 			for t in soup.findAll(text=True):
-					self.jobtitles.append(t.encode('utf-8'))
+					self.jobPostBody.append(t.encode('utf-8'))
 			#tagList = ["p","li","a"]
 			# soup = BeautifulSoup(data);
 			# for tag in tagList:

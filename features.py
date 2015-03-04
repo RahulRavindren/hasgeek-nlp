cbf9f48a1fe9ddf1c3642a04deec0a8da514efcd
Rahul Ravindran <rahul.r945@gmail.com>
2015-03-04 21:14:52 +0530

    added max likelyhood estimator

---
+++ features.py
@@ -0,0 +1,12 @@
+#-*- author: Rahul Ravindran
+#-*- coding:UTF-8
+
+class FeatureExtraction():
+	"""
+	Feature extraction for both post.title as well as post.body
+	"""
+	def __init__(self):
+		return
+
+	def maxLikelyHood(self):
+		
